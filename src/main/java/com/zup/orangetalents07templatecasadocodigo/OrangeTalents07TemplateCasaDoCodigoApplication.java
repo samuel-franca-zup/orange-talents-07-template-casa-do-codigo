45739094a3124bec9257e6a88c3bc3bb07a45681
samuel-leite-zup <samuel.leite@zup.com.br>
2022-04-22 23:29:08 -0300

    Initial commit

---
+++ src/main/java/com/zup/orangetalents07templatecasadocodigo/OrangeTalents07TemplateCasaDoCodigoApplication.java
@@ -0,0 +1,13 @@
+package com.zup.orangetalents07templatecasadocodigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangeTalents07TemplateCasaDoCodigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrangeTalents07TemplateCasaDoCodigoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zup/orangetalents07templatecasadocodigo/OrangeTalents07TemplateCasaDoCodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zup.orangetalents07templatecasadocodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrangeTalents07TemplateCasaDoCodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
